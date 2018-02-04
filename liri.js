3106e3537f31a6457c5aae5f4d031460d2aa7ff1
Brian Parkes <parkesbrianj@gmail.com>
2018-02-04 16:24:15 -0600

    coding user side functionality. Currently debugging between scenarios where do-what-it-says runs, user runs spotify without a defined query, and user runs with a defined query

--- liri.js
+++ liri.js
@@ -5,7 +5,9 @@ var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require('request');
 
-//my-tweets----------------node liri.js my-tweets--------------------------------//
+var userChoice = true; //True = user input specific instruction, false = user input "do-what-it-says"
+
+//my tweets----------------node liri.js my-tweets--------------------------------//
 function myTweets() {
     var client = new Twitter(keys.twitter);
     client.get('statuses/user_timeline', {q: 'node.js', count: 20}, function(error, tweets, response) {
@@ -23,18 +25,26 @@ function myTweets() {
         }
     });
 }
-//-------------------------------------------------------------------------------//
+//------------------------------------------------------------------------------//
 
-//spotify-this-song--------node liri.js spotify-this-song '<song name here>'-----//
-function spotifyThisSong() {
+//spotify this song--------node liri.js spotify-this-song '<song name here>'-----//
+function spotifyThisSong(random, userChoice) {
     var spotify = new Spotify(keys.spotify);
-    
+    //Set to random for "do-what-it-says"
+    if(userChoice === false) {
+        var track = random;
+    }
+    //or set to user input
+    else {
+        var track = process.argv[3];
+    }
+
     //Set default song and/or enter user input into variable
     if (process.argv[2] === undefined) {
         useDefaultSong();
     }
     else {
-        spotifySearch(process.argv[3]);
+        spotifySearch(track);
     }
     
     //search: function({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
@@ -58,61 +68,88 @@ function spotifyThisSong() {
             
     function useDefaultSong() {
         var defaultSong;
-        fs.readFile(`random.txt`, `utf8`, function(error, data) {
+        fs.readFile(`defaultSpotifySong.txt`, `utf8`, function(error, data) {
             defaultSong = data.split(`"`)[1];
             spotifySearch(defaultSong);
         });
     }
     function displaySpotifyInfo(artist, track, link, album) {
-        console.log(`${artist}`);
-        console.log(`${album}`);
-        console.log(`${track}`);
-        console.log(`${link}`);
+        console.log(`Artist:        ${artist}`);
+        console.log(`Album:         ${album}`);
+        console.log(`Song:          ${track}`);
+        console.log(`Preview Link:  ${link}`);
     }
 }
-//-------------------------------------------------------------------------------//
-
-//movie-this---------------node liri.js movie-this '<movie name here>'-----------//
-var movieToSearch = process.argv[2];
-var requestURLOMDB = `http://www.omdbapi.com/?apikey=${keys.OMDB.key}&t=${movieToSearch}`
-request(requestURLOMDB, function (error, response, body) {
-    if (response.statusCode != 200) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    }
-    else {
-        //Parse response
-        var movieResponse = JSON.parse(body);
-        //Get all wanted values from JSON response
-        var title = movieResponse.Title;
-        var year = movieResponse.Year;
-        for (var i = 0; i < movieResponse.Ratings.length; i++) {
-            var thisOne = movieResponse.Ratings[i];
-            if (thisOne.Source === `Internet Movie Database`) {
-                var ratingIMDB = thisOne.Value;
-            }
-            else if (thisOne.Source === `Rotten Tomatoes`) {
-                var ratingRT = thisOne.Value;
-            }
+//------------------------------------------------------------------------------//
+//movie this---------------node liri.js movie-this '<movie name here>'-----------//
+function movieThis() {
+    var movieToSearch = process.argv[2];
+    var requestURLOMDB = `http://www.omdbapi.com/?apikey=${keys.OMDB.key}&t=${movieToSearch}`
+    request(requestURLOMDB, function (error, response, body) {
+        if (response.statusCode != 200) {
+            console.log('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         }
-        var country = movieResponse.Country;
-        var language = movieResponse.Language;
-        var plot = movieResponse.Plot;
-        var actors = movieResponse.Actors;
-        displayOMDBInfo(title, year, ratingIMDB, ratingRT, country, language, plot, actors);
+        else {
+            //Parse response
+            var movieResponse = JSON.parse(body);
+            //Get all wanted values from JSON response
+            var title = movieResponse.Title;
+            var year = movieResponse.Year;
+            for (var i = 0; i < movieResponse.Ratings.length; i++) {
+                var thisOne = movieResponse.Ratings[i];
+                if (thisOne.Source === `Internet Movie Database`) {
+                    var ratingIMDB = thisOne.Value;
+                }
+                else if (thisOne.Source === `Rotten Tomatoes`) {
+                    var ratingRT = thisOne.Value;
+                }
+            }
+            var country = movieResponse.Country;
+            var language = movieResponse.Language;
+            var plot = movieResponse.Plot;
+            var actors = movieResponse.Actors;
+            displayOMDBInfo(title, year, ratingIMDB, ratingRT, country, language, plot, actors);
+        }
+    });
+    function displayOMDBInfo(title, year, ratingIMDB, ratingRT, country, language, plot, actors) {
+        console.log(`Title:                     ${title}`);
+        console.log(`Year:                      ${year}`);
+        console.log(`IMDB Rating:               ${ratingIMDB}`);
+        console.log(`Rotten Tomatoes Rating:    ${ratingRT}`);
+        console.log(`Country:                   ${country}`);
+        console.log(`Language:                  ${language}`);
+        console.log(`Plot:                      ${plot}`);
+        console.log(`Actors:                    ${actors}`);
     }
-});
-function displayOMDBInfo(title, year, ratingIMDB, ratingRT, country, language, plot, actors) {
-    console.log(`Title:                     ${title}`);
-    console.log(`Year:                      ${year}`);
-    console.log(`IMDB Rating:               ${ratingIMDB}`);
-    console.log(`Rotten Tomatoes Rating:    ${ratingRT}`);
-    console.log(`Country:                   ${country}`);
-    console.log(`Language:                  ${language}`);
-    console.log(`Plot:                      ${plot}`);
-    console.log(`Actors:                    ${actors}`);
 }
 //-------------------------------------------------------------------------------//
+//do what it says----------node liri.js do-what-it-says-------------------------//
 
-//do-what-it-says----------node liri.js do-what-it-says--------------------------//
+//-------------------------------------------------------------------------------//
+//Run the called code-----------------------------------------------------------//
+var userRequest = process.argv[2];
+var searchItem = process.argv[3];
+if (userRequest === `do-what-it-says`) {
+    userChoice = false;
+    fs.readFile(`random.txt`, `utf8`, function(error, data) {
+        userRequest = data.split(`,`)[0];
+        searchItem = data.split(`,`)[1];
+        userRequest = userRequest.trim();
+        checkWhichOneToRun(userRequest, searchItem, userChoice);
+    });
+}
+
+function checkWhichOneToRun(userRequest, searchItem, userChoice) {
+    if (userRequest === `my-tweets`) {
+        myTweets(searchItem, userChoice);
+    }
+    else if(userRequest === `spotify-this-song`) {
+        console.log('got here');
+        spotifyThisSong(searchItem, userChoice);
+    }
+    else if (userRequest === `movie-this`) {
+        movieThis(searchItem, userChoice);
+    }
+}
 //-------------------------------------------------------------------------------//
