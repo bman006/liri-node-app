ab005ac01f3d18388c598b09b4f6ed697150b135
Brian Parkes <parkesbrianj@gmail.com>
2018-02-04 13:44:41 -0600

    added code to store either a default song name or the user entered song name from the command line

--- liri.js
+++ liri.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 var keys = require(`./keys.js`);
 var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var fs = require(`fs`);
 
-//var spotify = new Spotify(keys.spotify);
 
 
-//my-tweets----------------node liri.js my-tweets--------------------------------
+
+//my-tweets----------------node liri.js my-tweets--------------------------------//
 function myTweets() {
     var client = new Twitter(keys.twitter);
     client.get('statuses/user_timeline', {q: 'node.js', count: 20}, function(error, tweets, response) {
@@ -23,7 +25,45 @@ function myTweets() {
         }
     });
 }
-//spotify-this-song--------node liri.js spotify-this-song '<song name here>'-----
+//-------------------------------------------------------------------------------//
+
+//spotify-this-song--------node liri.js spotify-this-song '<song name here>'-----//
+var spotify = new Spotify(keys.spotify);
+
+//Set default song and/or enter user input into variable
+var songToSearch = ``;
+if (process.argv[3] === undefined) {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        songToSearch = data.split(`"`)[1];
+    });
+}
+else {
+    songToSearch = process.argv[3];
+}
+
+//search: function({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data); 
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //movie-this---------------node liri.js movie-this '<movie name here>'-----------
 //do-what-it-says----------node liri.js do-what-it-says--------------------------
